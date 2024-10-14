839e2ad6f65dfab8abb5b55e86df9fe06d3456fc
Hatem <hatem.deux@gmail.com>
2024-10-14 00:35:49 -0400

    write test data testUsers testUrlDatabase

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -5,3 +5,40 @@ const {
   generateRandomString,
   urlsForUser
 } = require('../helper.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+const testUrlDatabase = {
+  b2xVn2: {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "userRandomID",
+  },
+  b2xVn3: {
+    longURL: "http://www.metro.ca",
+    userID: "userRandomID",
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "user2RandomID",
+  },
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
