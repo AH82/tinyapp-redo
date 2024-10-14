5c3f315d07c225aaa4a5627c47ef2ff26a2e5dcc
Hatem <hatem.deux@gmail.com>
2024-10-13 21:35:26 -0400

    modularize: move fn generateRandomString()  ('express_server.js' -> 'helper.js')

--- express_server.js
+++ express_server.js
@@ -3,6 +3,10 @@ const {
   urlDatabase
 } = require("./databases.js");
 
+const {
+  generateRandomString
+} = require("./helper.js");
+
 const express = require("express");
 const cookieSession = require("cookie-session");
 const bcrypt = require("bcryptjs");
@@ -21,42 +25,7 @@ app.use(cookieSession({
   maxAge: 2 * 24 * 60 * 60 * 1000 // 48 hours
 }));
 
-/**
- * generates random strings for "unique" IDs.
- * @param {number} strLength Desired String Length. Default = 6.
- * @returns {string} generated random string.
- */
-const generateRandomString = function(strLength = 6) {
 
-  if (typeof(strLength) !== "number")
-    throw new TypeError("Argument must be of type Number");
-
-  let randomString = "";
-
-  //alphanumeric possibilities
-  const charPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  + "abcdefghijklmnopqrstuvwxyz" + "0123456789";
-   
-  /* random # between 0-1, multiplied by the number of possible characters,
-     rounded then character at this position, then pushed into the string. */
-  for (let i = 0; i < strLength; i++)
-    randomString += charPool.charAt(Math.floor(Math.random() * charPool.length));
-   
-  return randomString;
-};
-
-// MINI_TEST : for generateRandomString() fn
-/* // HAPPY PATH TEST
-console.log(
-  "Test: random string is => ",
-  generateRandomString()
-); */
-/*
-// NEGATIVE-TEST :
-console.log(
-  "Test: random string is => ",
-  generateRandomString("true")
-); */
 
 
 /**
